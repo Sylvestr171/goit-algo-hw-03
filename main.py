2010a2ae26bd34e883eb5cf2c624e8e9db7c8fab
Oleksii Haiduk <Silvestr171@gmail.com>
2025-05-09 15:00:01 +0300

    перше завдання без однієї вимоги

--- main.py
+++ main.py
@@ -1,41 +1,14 @@
 from datetime import datetime
 
-"""
-***Перше завдання***
-Створіть функцію get_days_from_today(date), яка розраховує кількість днів між заданою датою і поточною датою.
-
-Вимоги до завдання:
-Функція приймає один параметр: date — рядок, що представляє дату у форматі 'РРРР-ММ-ДД' (наприклад, '2020-10-09').
-Функція повертає ціле число, яке вказує на кількість днів від заданої дати до поточної. Якщо задана дата пізніша за поточну, результат має бути від'ємним.
-У розрахунках необхідно враховувати лише дні, ігноруючи час (години, хвилини, секунди).
-Для роботи з датами слід використовувати модуль datetime Python.
-
-Рекомендації для виконання:
-Імпортуйте модуль datetime.
-Перетворіть рядок дати у форматі 'РРРР-ММ-ДД' у об'єкт datetime.
-Отримайте поточну дату, використовуючи datetime.today().
-Розрахуйте різницю між поточною датою та заданою датою.
-Поверніть різницю у днях як ціле число.
-
-Критерії оцінювання:
-Коректність роботи функції: функція повинна точно обраховувати кількість днів між датами.
-Обробка винятків: функція має впоратися з неправильним форматом вхідних даних.
-Читабельність коду: код повинен бути чистим і добре документованим.
-
-Приклад:
-Якщо сьогодні 5 травня 2021 року, виклик get_days_from_today("2021-10-09") повинен повернути 157, оскільки 9 жовтня 2021 року є на 157 днів пізніше від 5 травня 2021 року.
-
-"""
-
-
 # Function for calculating the number of days between two dates
 def get_days_from_today(date:str) -> int:
+    
     """
     Function for calculating the number of days between two dates:
         - takes one parameter in date format 'YYYY-MM-DD';
         - outputs integer number of days betwen some days to today (if the some date is later then the current one, the result is negative);
         - only days are taken into account, hours, min. etc. are ignored;
-        ***- function should handle incorrect input data format.
+     ****НАРАЗІ НЕ ЗРОЗУМІВ ПОВЕРНУТИСЬ ПІЗНІШЕ*****- function should handle incorrect input data format.
 
     Input:
     :param date: string
@@ -43,11 +16,15 @@ def get_days_from_today(date:str) -> int:
     Output:
     :return: integer
     """
-    date_in_datetime = (datetime.strptime(date, '%Y-%m-%d').date())
-    today = datetime.now().date()
-    print (date_in_datetime, type(date_in_datetime), today, type(today), sep='\t-----\t')
 
-get_days_from_today('2020-10-09')
+    date_in_datetime = (datetime.strptime(date, '%Y-%m-%d').date())
+#    today_date = datetime.strptime('2021.05.05', '%Y.%m.%d').date() #для вказання дати при перевірці
+    today_date = datetime.now().date()
+    diff_day = (date_in_datetime - today_date).days
+#    print (date_in_datetime, type(date_in_datetime), today_date, type(today_date), diff_day, type(diff_day), sep='\t-----\t') #для перевірки роботи
+    return diff_day
+
+get_days_from_today('2021-10-09')
 
 
 """
